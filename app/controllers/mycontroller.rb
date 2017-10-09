644d97bda60015b4ec70b4c7ad56a3ca019334d6
1412033 <lhb.bvl@gmail.com>
2017-10-09 22:25:40 +0700

    fix up host

--- app/controllers/mycontroller.rb
+++
@@ -1,9 +0,0 @@
-class MyController
-  add_breadcrumb "home", :root_path
-  add_breadcrumb "my", :my_path
-
-  def index
-    add_breadcrumb "homepage", "/"
-    add_breadcrumb "index", index_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
+root 'sessions#new'
 	resources :users, only: [:new, :create]
-	
+
 	get '/sessions/login', to: 'sessions#new', as: :login
   post '/sessions/login', to: 'sessions#create', as: nil
   delete '/sessions/logout', to: 'sessions#destroy', as: :logout
